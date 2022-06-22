98f4a684c3ca5be34a82935cc934317fe0558d8f
Simon Berger <simon.c.berger@gmail.com>
2022-06-22 07:56:07 +0200

    Use filesystem::path for multipart files

--- include/cpr/multipart.h
+++ include/cpr/multipart.h
@@ -7,19 +7,21 @@
 #include <type_traits>
 #include <vector>
 
+#include <cpr/filesystem.h>
+
 namespace cpr {
 
 struct File {
-    explicit File(std::string&& p_filepath) : filepath(std::move(p_filepath)) {}
-    explicit File(const std::string& p_filepath) : filepath(p_filepath) {}
-    const std::string filepath;
+    explicit File(fs::path&& p_filepath) : filepath(std::move(p_filepath)) {}
+    explicit File(const fs::path& p_filepath) : filepath(p_filepath) {}
+    const fs::path filepath;
 };
 
 struct Buffer {
     using data_t = const unsigned char*;
 
     template <typename Iterator>
-    Buffer(Iterator begin, Iterator end, std::string&& p_filename)
+    Buffer(Iterator begin, Iterator end, fs::path&& p_filename)
             // Ignored here since libcurl reqires a long.
             // There is also no way around the reinterpret_cast.
             // NOLINTNEXTLINE(google-runtime-int, cppcoreguidelines-pro-type-reinterpret-cast)
@@ -35,17 +37,18 @@ struct Buffer {
     // Ignored here since libcurl reqires a long:
     // NOLINTNEXTLINE(google-runtime-int)
     long datalen;
-    const std::string filename;
+    const fs::path filename;
 };
 
 struct Part {
     Part(const std::string& p_name, const std::string& p_value, const std::string& p_content_type = {}) : name{p_name}, value{p_value}, content_type{p_content_type}, is_file{false}, is_buffer{false}, has_filename{false} {}
     Part(const std::string& p_name, const std::int32_t& p_value, const std::string& p_content_type = {}) : name{p_name}, value{std::to_string(p_value)}, content_type{p_content_type}, is_file{false}, is_buffer{false}, has_filename{false} {}
-    Part(const std::string& p_name, const File& file, const std::string& p_content_type = {}) : name{p_name}, value{file.filepath}, content_type{p_content_type}, is_file{true}, is_buffer{false}, has_filename{false} {}
-    Part(const std::string& p_name, const std::string& p_filename, const File& file, const std::string& p_content_type = {}) : name{p_name}, filename{p_filename}, value{file.filepath}, content_type{p_content_type}, is_file{true}, is_buffer{false}, has_filename{true} {}
-    Part(const std::string& p_name, const Buffer& buffer, const std::string& p_content_type = {}) : name{p_name}, value{buffer.filename}, content_type{p_content_type}, data{buffer.data}, datalen{buffer.datalen}, is_file{false}, is_buffer{true}, has_filename{false} {}
+    Part(const std::string& p_name, const File& file, const std::string& p_content_type = {}) : name{p_name}, value{file.filepath.string()}, content_type{p_content_type}, is_file{true}, is_buffer{false}, has_filename{false} {}
+    Part(const std::string& p_name, const fs::path& p_filename, const File& file, const std::string& p_content_type = {}) : name{p_name}, filename{p_filename}, value{file.filepath.string()}, content_type{p_content_type}, is_file{true}, is_buffer{false}, has_filename{true} {}
+    Part(const std::string& p_name, const Buffer& buffer, const std::string& p_content_type = {}) : name{p_name}, value{buffer.filename.string()}, content_type{p_content_type}, data{buffer.data}, datalen{buffer.datalen}, is_file{false}, is_buffer{true}, has_filename{false} {}
 
     std::string name;
+    // We don't use fs::path here, as this leads to problems using windows
     std::string filename;
     std::string value;
     std::string content_type;
